e78bdc59809bc6bd68513dd0f36c4a792923a314
anthonyprograms <anthony.williams@student.csulb.edu>
2017-05-12 15:07:44 -0700

    Comments

--- Calculator.java
+++ Calculator.java
@@ -31,6 +31,9 @@ public class Calculator
         return selection;
     }
 
+    // Takes the selection entered by the user
+    // Asks for the complex numbers to be entered
+    // And calls the appropriate method
     public static void handleSelection(Scanner input, int selection) {
         Numbers nums;
 
@@ -57,6 +60,11 @@ public class Calculator
         }
     }
 
+    // Gets the complex numbers entered in by the user
+    // Uses the boolean to decide whether 1 or 2 complex numbers are needed
+    // 1 complex number for mag & angle
+    // 2 complex numbers for add & div
+    // Returns an object of the entered numbers to be computed
     public static Numbers getComplexValues(Scanner input, Boolean needsTwo) {
         Numbers nums = new Numbers();
 
@@ -80,6 +88,7 @@ public class Calculator
         return nums;
     }
 
+    // Adds complex numbers
     public static void add(Numbers nums) {
         double first = nums.x + nums.u;
         double second = nums.y + nums.v;
@@ -87,6 +96,7 @@ public class Calculator
         System.out.printf("(%.1f + %.1fi) + (%.1f + %.1fi) = %.1f + %.1fi\n", nums.x, nums.y, nums.u, nums.v, first, second);
     }
 
+    // Divides complex numbers
     public static void div(Numbers nums) {
         double firstNum = nums.x * nums.u + nums.y * nums.v;
         double firstDen = nums.u * nums.u + nums.v * nums.v;
@@ -99,13 +109,14 @@ public class Calculator
         System.out.printf("(%.1f + %.1fi) / (%.1f + %.1fi) = %.1f + %.1fi\n", nums.x, nums.y, nums.u, nums.v, firstResult, secondResult);
     }
 
+    // Takes the magnitude of a complex number
     public static void mag(Numbers nums) {
         double result = Math.sqrt(nums.x * nums.x + nums.y * nums.y);   
 
         System.out.printf("| (%.1f + %.1fi) | = %.1f\n", nums.x, nums.y, result);  
     }
 
-    // Fix arc tan
+    // Gets the angle of a complex number
     public static void ang(Numbers nums) {
         double result = Math.atan(nums.y / nums.x);
         System.out.printf("ang (%.1f + %.1fi) = %.1f\n", nums.x, nums.y, result); 
